30e33d5f0b9f9c000b77d42ee42af711c0923e7b
Adivhahomaphisa <Adivhahomaphisa@gmail.com>
2023-08-14 08:14:48 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,12 +8,15 @@
 int main(void)
 {
 	int i;
-	char j;
 
-	for (i = 0 ; i < 10 ; i++)
-		putchar (i + '0');
-	for (j = 'a' ; j <= 'f' ; i++)
-		putchar(j);
+	for (i = 48 ; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97 ; i < 103 ; i++)
+	{
+		putchar(i);
+	}
 	putchar('\n');
 	return (0);
 }
