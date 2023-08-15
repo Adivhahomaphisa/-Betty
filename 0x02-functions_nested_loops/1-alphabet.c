97ec2f3ef3bea207b7c23b997e6629c982675145
Adivhahomaphisa <Adivhahomaphisa@gmail.com>
2023-08-15 13:24:29 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print all alphabet in lowercase
+ *
+ * Return: always 0
+ */
+int main(void)
+
+	char a, z;
+
+	for (letter = 'a' ; letter <= 'z' ; letter)
+	_putchar(letter);
+	_putchar('\n');
+	return (0);
+}
